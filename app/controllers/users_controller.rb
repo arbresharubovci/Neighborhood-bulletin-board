8259b42b6c104676ab0a2e89fc0cdef256f117c3
Arbreshë Rubovci <arbresharubovci@gmail.com>
2023-03-16 21:29:45 +0000

    The form for creating the post is done, also show deatels link.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ActionController::Base
 
 def index
+  #Parameters: {"input_username"=>"new"}
+  #input_username = params.fetch("input_username"=>"post_id")
+  matching_posts = Post.all
+  @list_of_posts = matching_posts.order({ :username => :asc})
   render({ :template => "users_templates/index.html.erb" })
+end
+
+def show
+  render({ :template => "users_templates/show.html.erb" })
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 get("/home" , { :controller => "users" , :action => "index"})
+get("/home/post_show" , { :controller => "users" , :action => "show"})
 
 get("/posts" , { :controller => "posts" , :action => "index"})
 
